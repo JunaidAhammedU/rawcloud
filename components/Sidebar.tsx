b2d8290d993458beff4f67e80e57c43dde75963d
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-16 12:37:29 +0530

    feat(sidebar): add sidebar component with navigation links and logo feat(header): integrate search and file uploader components in header feat(constants): create constants for navigation items and actions dropdown

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import { navItems } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>sas</div>;
+  const pathName = usePathname();
+  return (
+    <aside className="sidebar">
+      <Link href="#" className="sidebar">
+        <Image
+          src="/accets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto md:block"
+        />
+
+        <Image
+          src="/accets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+
+        <nav className="sidebar-nav">
+          <ul className="flex fle-1 flex-col gap-6">
+            {navItems.map(({ url, name, icon }) => {
+              const active = pathName === url;
+              return (
+                <Link href={url} key={name}>
+                  <li>
+                    <Image src={icon} alt={name} width={24} height={24} />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
+      </Link>
+    </aside>
+  );
 };
 
 export default Sidebar;
