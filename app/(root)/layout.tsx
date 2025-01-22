57dc8bf85d1dfed70daeae6305ae348e3cdb3bfb
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-22 12:07:08 +0530

    refactor(layout): simplify user authentication check in layout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,13 +5,8 @@ import Sidebar from "@/components/Sidebar";
 import { getCurrentUser } from "@/lib/actions/user.action";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-  try {
-    const currentUser = await getCurrentUser();
-    if (!currentUser) redirect("/sign-in");
-  } catch (error) {
-    redirect("/sign-in");
-  }
-
+  const currentUser = await getCurrentUser();
+  if (!currentUser) redirect("/sign-in");
   return (
     <main className="flex h-screen">
       <Sidebar />
