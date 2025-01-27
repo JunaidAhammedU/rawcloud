18a08261f17a6774eda7be3b27668480b6e3a2d7
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-27 10:59:51 +0530

    feat(ui): improve type safety in Search component and refine state management

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,19 +5,18 @@ import React, { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import { useSearchParams } from "next/navigation";
 import { getFile } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
 
 const Search = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState<string>("");
   const searchParams = useSearchParams();
-  const searchQuery = searchParams.get("query") || "";
-  const [result, setResult] = useState([]);
+  const searchQuery: any = searchParams.get("query") || "";
+  const [result, setResult] = useState<Models.Document[]>([]);
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const fetchFiles = async () => {
-      const files = await getFile({
-        searchText: query,
-      });
+      const files = await getFile({ searchText: query });
       setResult(files.documents);
       setOpen(true);
     };
