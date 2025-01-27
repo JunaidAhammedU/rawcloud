9d2c23031cf99731bf5c00edeb8e54aa728d055c
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-27 11:10:13 +0530

    feat(ui): enhance Search component with query-based file fetching and thumbnail display

--- components/Search.tsx
+++ components/Search.tsx
@@ -6,6 +6,7 @@ import { Input } from "./ui/input";
 import { useSearchParams } from "next/navigation";
 import { getFile } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
 
 const Search = () => {
   const [query, setQuery] = useState<string>("");
@@ -21,7 +22,7 @@ const Search = () => {
       setOpen(true);
     };
     fetchFiles();
-  }, []);
+  }, [query]);
 
   useEffect(() => {
     if (!searchQuery) {
@@ -45,7 +46,32 @@ const Search = () => {
           onChange={(e) => setQuery(e.target.value)}
         />
 
-        {open && <ul></ul>}
+        {open && (
+          <ul className="search-result">
+            {result.length > 0 ? (
+              result.map((file) => (
+                <li
+                  key={file.$id}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file?.type}
+                      extension={file.extension}
+                      url={file?.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No results found</p>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
