f0ab5b2520f222a07e50334afc0f9c766a98b44e
JunaidAhammedU <junaidahammed15@gmail.com>
2025-02-05 11:25:46 +0530

    refactor(ActionDropdown): remove outdated comment from closeAllModel function

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -46,7 +46,6 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
   const [name, setName] = useState(file?.name);
   const [loading, setLoading] = useState(false);
 
-  // Close all modals and dropdowns
   const closeAllModel = () => {
     setIsModalOpen(false);
     setIsDropdownOpen(false);
