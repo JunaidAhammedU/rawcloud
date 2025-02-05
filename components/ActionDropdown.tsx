c89ba1b90e63efa6db2dc0d361d7d1bed96a41ec
JunaidAhammedU <junaidahammed15@gmail.com>
2025-02-05 11:26:47 +0530

    refactor(ActionDropdown): add comment to clarify handleRemoveUser function

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -87,6 +87,7 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
     setLoading(false);
   };
 
+  // Handle remove user
   const handleRemoveUser = async (email: string) => {
     const updatedEmails = emails.filter((e) => e !== email);
     const response = await updateFileUsers({
