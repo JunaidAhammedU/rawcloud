343cd6dd48915bfb3aef38a700529e6abbdd84c9
JunaidAhammedU <junaidahammed15@gmail.com>
2025-02-14 10:57:28 +0530

    feat(Navigation): add margin and background color to FileUploader container in MobileNavigation

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -122,7 +122,7 @@ const MobileNavigation = ({
           <StorageUsage used={formatStorageSize(usage?.storageUsage) || 0} />
 
           <div className="flex flex-col justify-between gap-5">
-            <div>
+            <div className="mt-2 bg-red">
               <FileUploader ownerId={ownerId} accountId={accountId} />
             </div>
             <Button
