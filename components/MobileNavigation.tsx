089491ae5ea45da373358c34539e5b631037c2ad
JunaidAhammedU <junaidahammed15@gmail.com>
2025-02-14 11:00:48 +0530

    feat(Navigation): add padding and center alignment to FileUploader container in MobileNavigation

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -122,7 +122,7 @@ const MobileNavigation = ({
           <StorageUsage used={formatStorageSize(usage?.storageUsage) || 0} />
 
           <div className="flex flex-col justify-between gap-5">
-            <div className="flex items-center">
+            <div className="flex p-2 pt-3 items-center justify-center">
               <FileUploader ownerId={ownerId} accountId={accountId} />
             </div>
             <Button
